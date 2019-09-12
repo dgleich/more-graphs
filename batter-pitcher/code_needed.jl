0f2899530e1400fa32a6156335ec981ab6b866b8
Huda Nassar <hnassar@purdue.edu>
2019-09-12 15:50:22 -0400

    batter-pitcher-data

---
+++ batter-pitcher/code_needed.jl
@@ -0,0 +1,49 @@
+using DelimitedFiles
+
+# write_to_smat
+function writeToSMAT(M,filename)
+    if size(M,2) == 2
+        M = hcat(M,ones(Int,size(M,1)))
+    end
+    m = maximum(M)+1
+    n = maximum(M)+1
+    nz = size(M,1)
+    open(filename, "w") do f
+        write(f, "$m   $n   $nz \n")
+        writedlm(f,M)
+    end
+end
+function findin_index(x,y)
+     indices_in_y = zeros(Int64,length(x))
+     already_exist = findall((in)(y), x)
+     donot_exist = setdiff(1:length(x),already_exist)
+     funcmap = i -> indices_in_y[findall(x.==y[i])] .= i
+     lookfor_indices = findall((in)(x), y)
+     map(funcmap,lookfor_indices)
+     return indices_in_y
+end
+
+function run_script()
+    M = readdlm("playerinfo.txt",header=true)
+    names = M[1][:,3].*" ".*M[1][:,2]
+    name_ids = M[1][:,1]
+    names_data = hcat(name_ids,names)
+
+    allfiles = readdir("pitcherinfo")
+    edge_list = Array{Int64}(undef,0,2)
+    for i = 1:length(allfiles)
+        fileid = allfiles[i]
+        Mi = readdlm(joinpath("pitcherinfo",fileid))
+        # 1(skip) - 20 - 1(skip) - 20 
+        # the crawler needs to change to not crawl "pitcher" every 20 lines
+        interestingids = findall(Mi[:,1].!="Pitcher")
+        curfullnames = Mi[interestingids,1].*" ".*Mi[interestingids,2]
+        indexnumbers = findin_index(curfullnames,names_data[:,2])
+        connectto = indexnumbers[findall(indexnumbers.!=0)]
+        curplayerid = fileid[1:findfirst("_",fileid)[1]-1]
+        curplayerid_inlist = findfirst(name_ids.==curplayerid)
+        new_connections = hcat(ones(Int64,length(connectto)).*curplayerid_inlist,connectto)
+        edge_list = vcat(edge_list,new_connections)
+    end
+return edge_list,names_data
+end
---
+++ batter-pitcher/readme.md
@@ -0,0 +1,26 @@
+### Quick notes about this data:
+## To regenerate:
+- Data was retrieved from: https://github.com/nassarhuda/data_crawls/tree/master/batter_pitcher
+- All relevant information about the data and crawler should be found in this repo.
+- If you download the above repo, you will have the data needed.
+- Place `code_needed.jl` and `script.jl` in the same folder and run `include("script.jl")` from a julia session.
+- This should generate the `.smat` and `.labels` files.
+
+## Files
+- `batter_pitcher_names.labels`: Labels are arranged in order, i.e. node `i` in the network corresponds to row `i` in the labels file.
+- `batter_pitcher_edges.smat`: smat file of batter pitcher plays, where players are indexed from zero. 
+Top of the file looks like:
+```
+19952   19952   815493 
+1	12	1
+1	26	1
+1	47	1
+1	51	1
+1	174	1
+1	326	1
+1	317	1
+1	362	1
+1	402	1
+
+```
+The top line indicates that there are 19952 nodes, and 815493 edges. The rest of the lines are `<batterid> <pitcherid> 1`. This is an unweighted graph, and thus the weights are understood to be 1.
