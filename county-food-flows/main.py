9ef67b72a32b779d6abdf9e8110430c7b39dd1f3
Omar Eldaghar <oeldagha@purdue.edu>
2019-11-22 16:01:46 -0500

    modified code and data

--- county-food-flows/main.py
+++ county-food-flows/main.py
@@ -3,7 +3,6 @@
 
 
 #remeber to cite the paper!
-
 import csv
 
 edges = set()
@@ -25,27 +24,10 @@ with open(fpath,'r') as fptr:
         nodes.add(src)
         nodes.add(dst)
         
-nodes = list(nodes)    
-
-#mapping nodes and using fips name
-#manually added a few entries in sorted order to file
-#data for file obtained psuedo manually from https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt
-#it was corrected via info at https://www.census.gov/geographies/reference-files/2018/demo/popest/2018-fips.html
-#08014 Broomfield County
-#12086 Miami-Dade County
-#46102 Oglala Lakota County
-
-cpath= r'counties.txt'
-reader = csv.reader(open(cpath,'r'))
-newData = dict()
-for item in reader:
-    if item[0][-3:]=='000':
-        state = item[1]
-    else:
-        newData[item[0]]=item[1]+','+' '+state
-        
+nodes = list(nodes)
+nodes.sort()
+ 
 nodeMap = {nodes[k]:k for k in range(len(nodes))}
-
 edges=list(map(lambda x:(nodeMap[x[0]],nodeMap[x[1]]),edges))
 
 symEdges = set()
@@ -53,12 +35,39 @@ for k in edges:
     symEdges.add(k)
     symEdges.add((k[1],k[0]))        
 
+
+#requires data from https://github.com/dgleich/more-graphs/tree/master/misc-county-fips-data
+#which contains same data as 
+tab = dict()
+with open('county-data.txt','r',encoding='utf-8') as fptr:
+    reader = csv.reader(fptr,delimiter='\t')
+    header = next(reader)
+    stateInd = header.index('state')
+    fipsInd = header.index('fips')
+    countyInd = header.index('county')
+    latInd = header.index('latitude')
+    lonInd = header.index('longitude')
+    
+    for item in reader:
+        tab[item[fipsInd]] = [item[countyInd]+', '+item[stateInd],item[latInd],item[lonInd]]
+
+
 #writing files
-with open('counties.labels','w') as fptr:
-    for k,item in enumerate([(nodes[k],newData[nodes[k]]) for k in range(len(nodes))]):
+with open('counties.labels','w',encoding='utf-8') as fptr:
+    for item in [(nodes[k],tab[nodes[k]][0]) for k in range(len(nodes))]:
         fptr.write('{0:s}\t{1:s}\n'.format(item[0],item[1]))
     
+#this needs file found at     
+with open('counties.xy','w',encoding='utf-8') as fptr:
+    for item in [tab[nodes[k]][1:] for k in range(len(nodes))]:
+        fptr.write('{0:s}\t{1:s}\n'.format(item[0],item[1]))
+    
+with open('full-county-info.xy','w',encoding='utf-8') as fptr:
+    for k in nodes:
+        fptr.write('{0:s}\t{1:s}\t{2:s}\t{3:s}\n'.format(k,tab[k][0],tab[k][1],tab[k][2]))
+    
 edges.sort()
+
 with open('dir-county-flows.smat','w') as fptr:    
     fptr.write('{0:d}\t{1:d}\t{2:d}\n'.format(len(nodes),len(nodes),len(edges)))
     for item in edges:
@@ -69,4 +78,3 @@ with open('undir-county-flows.smat','w') as fptr:
     fptr.write('{0:d}\t{1:d}\t{2:d}\n'.format(len(nodes),len(nodes),len(symEdges)))
     for item in symEdges:
         fptr.write('{0:d}\t{1:d}\t{2:d}\n'.format(item[0],item[1],1))
-    
--- county-food-flows/readme.md
+++ county-food-flows/readme.md
@@ -1,19 +1,21 @@
 # Data Description 
 The data that we used came from the supplementary data from Xiaowen Lin et al 2019 Environ. Res. Lett.14 084011. An online version of the paper as well as supplementary data can be found at [https://iopscience.iop.org/article/10.1088/1748-9326/ab29ae#supplementarydata](https://iopscience.iop.org/article/10.1088/1748-9326/ab29ae#supplementarydata). 
 
-For the FIPS data that we used to generate the `counties.labels` file was obtained from [https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt](https://transition.fcc.gov/oet/info/maps/census/fips/fips.txt) and was modified using three entries from [https://www.census.gov/geographies/reference-files/2018/demo/popest/2018-fips.html](https://www.census.gov/geographies/reference-files/2018/demo/popest/2018-fips.html). Namely the entries: 08014 Broomfield County, 12086 Miami-Dade County ,46102 Oglala Lakota County. 
+For the FIPS data that we used to generate the `counties.labels` file was obtained from  [https://github.com/dgleich/more-graphs/tree/master/misc-county-fips-data](https://github.com/dgleich/more-graphs/tree/master/misc-county-fips-data).
 
-All files are placed into a common directory and the python script is run from that directory. 
-
-#### TODO specify how data was prepped for processing
 
 # Files
-* counties.labels
-    * This file contains the county info that corresponds to node data. Note that there is a header file and node indices begin at 0.
 * dir-county-flows.smat
     * edge (i,j) represents a food flow from county i to county j
 * undir-county-flows.smat
     * same graph as above but undirected
+* counties.labels
+    * This file contains the county info that corresponds to node data. The first line corresponds to node 0.
+* counties.xy
+    * represents xy coords (latitude and longitude) for each county. Note that the first entry corresponds to node 0
+* full-county-info.xy
+    * this is just a concatenation above the above two files for convenience and clarity. Note that the first line corresponds to node 0.
+
 
 ## Examples
-node 1555 corresponds to 'Barren County, Kentucky'. The edge (19,944) corresponds to a directed flow (or a flow in undirected case) from 'Delaware County, New York' to 'Norfolk County, Massachusetts'.
+node 1555 corresponds to 'Phelps, MO'. The edge (31,164) corresponds to a directed flow (or a flow in undirected case) from 'Greene, AL' to 'Randolph, AR'.
