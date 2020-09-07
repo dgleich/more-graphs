fb62f6e70ea2ae05f7b5cd9f2379cfe08f58722e
Vaastav Arora <arora74@purdue.edu>
2020-09-06 23:19:42 -0400

    functional on example code block

--- Julia-co-calling-graph/Julia-co-calling-graph.jl
+++ Julia-co-calling-graph/Julia-co-calling-graph.jl
@@ -5,7 +5,7 @@ using Printf
 pkg_dir = "/Users/vaastavarora/.julia/packages/"
 
 # this variable needs to be changed based on your own installation directory
-path = "/Users/vaastavarora/.juliapro/JuliaPro_v1.2.0-2/registries/JuliaPro"
+path = "/Users/vaastavarora/.julia/registries/General"
 packages_dict = TOML.parsefile(joinpath(path,"Registry.toml"))["packages"]
 # this variable needs to be changed based on your own installation directory
 const STDLIB_DIR = "/Applications/Julia-1.3.app/Contents/Resources/julia/share/julia/stdlib/v1.3/"
@@ -19,7 +19,6 @@ for (i, stdlib) in enumerate(STDLIBS)
         push!(std_pkg_names,proj["name"])
     end
 end
-
 
 
 # Set of functions found so far
@@ -40,44 +39,86 @@ function resolve_path(index)
     # read source file as string
     src = read(curr_file,String)
 
+    src_expr = Expr(:call, :+, 1, 1)
+
     # parse source file
     try
         src_expr = Meta.parse(src)
+        return src_expr
     catch err
         println("Error in file ",index,"/",length(std_pkg_names))
+        return Expr(:call, :+, 1, 1)
     end
 
-    return src_expr
 end
 
+prog = """
+function nngraph(idxs::Matrix{Int32})
+  n = size(idxs, 2)
+  ei = zeros(Int,0)
+  ej = copy(ei)
+  for i=1:n
+    for k=1:size(idxs,1)
+      push!(ei, i)
+      push!(ej, idxs[k,i])
+    end
+  end
+  A = sparse(ei,ej,1.0,n,n)
+  return max.(A,A')
+end
+"""
+
+match_set = Set([Symbol("call"), Symbol(".")])
+sets = []
 # Function for graph construction
 function build_graph(exp::Expr)
 
-    if exp.head == Symbol(":function")
-        if size(exp.args)>2
+    println(exp.head)
+    if exp.head == Symbol("function")
+        println("Function Found")
+        #print(size(exp.args))
+        if size(exp.args)[1]>2
             throw(Exception)
         end
 
+        t =  build_graph(exp.args[2])
+        sets = [sets; t]
+        return t
 
-        if exp.args[1] in found_functions
-            Nothing
-        else
-            push!(found_functions,exp.args[1])
-            function_graph[exp.args[1]] = []
-        end
-    else
-        for texp in exp.args
-            if typeof(texp) == Expr
-                build_graph(texp)
+    elseif exp.head in match_set
+        #println("found a call",exp.args[1])
+        t = Set([exp.args[1]])
+
+        for x in exp.args
+            if typeof(x) == Expr
+                println("about to call ",x," ",typeof(x))
+                union!(t,build_graph(x))
             end
         end
+
+        return t
+    else
+        t = Set()
+        for texp in exp.args
+            if typeof(texp) == Expr
+                println("about to call ",texp," ",typeof(texp))
+                union!(t,build_graph(texp))
+            end
+        end
+
+        return t
     end
 end
+
 
 # Iterate all files in std package
 for i = 1:length(std_pkg_names)
     exps = resolve_path(i)
-    build_graph(exps)
+    if typeof(exps) != Nothing
+        println(exps)
+        build_graph(exps)
+    end
+    #dump(exps)
 end
 
 
